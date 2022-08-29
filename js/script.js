1bd5dc434569886a9a1e46f277dff618b44579a6
Alexandre <alexandrefaria3012@gmail.com>
2022-08-29 08:47:17 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+const pokemonName = document.querySelector('.poke_name');
+const pokeNumber = document.querySelector('.poke_number');
+const pokeImage = document.querySelector('.pokemon');
+
 const fetchPokemon = async (pokemon) => {
-    const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
     const data = await apiResponse.json();
 
     return data;
 }
 
-fetchPokemon(25);
+const renderPokemon = async (pokemon) =>{
+    const data = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = data.name;
+    pokeNumber.innerHTML = data.id;
+}
+
+renderPokemon('25');
